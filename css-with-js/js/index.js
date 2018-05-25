0355ddbd3c6422ce7b4c8365d1ca189cf89c6767
Myhaylo Zaharyak <mishazaharyak@gmail.com>
2018-05-25 19:34:50 +0300

    fixed the problem

--- css-with-js/js/index.js
+++ css-with-js/js/index.js
@@ -2,6 +2,7 @@ const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate () {
     const suffix = this.dataset.sizing || '';
+    console.log(console.dir(this.dataset));
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
