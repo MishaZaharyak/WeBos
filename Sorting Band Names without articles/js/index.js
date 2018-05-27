fc4ae91d7219759a7e214603bbb6942a1be37f74
Myhaylo Zaharyak <mishazaharyak@gmail.com>
2018-05-27 19:57:24 +0300

    sort with RegExp

---
+++ Sorting Band Names without articles/js/index.js
@@ -0,0 +1,15 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce The Veil', 'Norma Jean',
+	'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romas', 'Counterparts',
+	'Oh Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function strip(bandName) {
+	return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+
+const sortedBand = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector('#bands').innerHTML = sortedBand.map(band =>
+	`<li>${band}</li>`).join('');
+
+
+console.log(sortedBand);
